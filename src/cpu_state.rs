c77274e6b980819093b1513fb017e3c8a5538f9b
Rohan Kapur <rohankapurdev@outlook.com>
2024-12-22 21:15:43 +0800

    add register helpers

--- src/cpu_state.rs
+++ src/cpu_state.rs
@@ -7,20 +7,53 @@ impl Register {
     pub fn new() -> Self {
         Register { value: 0 } // we initialize all registers to 0
     }
+
+    // Get full 16-bit value
+    pub fn get(&self) -> u16 {
+        self.value
+    }
+
+    // Set full 16-bit value
+    pub fn set(&mut self, value: u16) {
+        self.value = value;
+    }
+
+    // Get low 8-bit byte
+    pub fn get_low(&self) -> u8 {
+        (self.value & 0xFF) as u8
+    }
+
+    // Get high 8-bit byte
+    pub fn get_high(&self) -> u8 {
+        ((self.value >> 8) & 0xFF) as u8
+    }
+
+    // Set low 8-bit byte
+    pub fn set_low(&mut self, value: u8) {
+        self.value = (self.value & 0xFF00) | (value as u16);
+    }
+
+    // Set high 8-bit byte
+    pub fn set_high(&mut self, value: u8) {
+        self.value = (self.value & 0x00FF) | ((value as u16) << 8);
+    }
 }
 
 #[derive(Debug)]
 pub struct CpuState {
+    // General purpose registers
     pub ax: Register,
     pub bx: Register,
     pub cx: Register,
     pub dx: Register,
 
+    // Index registers
     pub si: Register,
     pub di: Register,
     pub bp: Register,
     pub sp: Register,
 
+    // Instruction pointer
     pub ip: Register,
 }
 
